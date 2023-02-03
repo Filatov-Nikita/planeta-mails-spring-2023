a3365989fa4a55a41d4b340888615ee35891d69f
nikita <nikita45454@gmail.com>
2023-02-03 21:44:03 +0300

    feat: добавляет сборку письма

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,51 @@
-const { src, dest, watch } = require('gulp');
-const pug = require('gulp-pug');
-const rename = require('gulp-rename');
+import gulp from 'gulp';
+import pug from 'gulp-pug';
+import rename from 'gulp-rename';
+import imagemin, { mozjpeg } from 'gulp-imagemin';
+import imageminPngquant from 'imagemin-pngquant';
+import inlineCss from 'gulp-inline-css';
+import bsync from 'browser-sync';
+import { deleteAsync } from 'del';
 
-const watchedFiles = 'views/**/*.pug';
-const viewsPath = 'views/**/mail-*.pug';
+const browserSync = bsync.create();
+
+const watchedFiles = 'src/views/**/*.{pug,css}';
+const viewsPath = 'src/views/**/mail-*.pug';
+const imagesPath = 'src/images/**/*';
 const distPath = 'dist/';
 
-exports.pug = function compilePug() {
-  return src(viewsPath)
-  .pipe(pug())
-  .pipe(rename({ extname: '.html' }))
-  .pipe(dest(distPath));
+function delImages() {
+  return deleteAsync(distPath + 'images');
 }
 
-exports.default = function () {
-  watch(watchedFiles, exports.pug);
+export async function images() {
+  await delImages();
+  return gulp.src(imagesPath)
+  .pipe(
+    imagemin([
+      mozjpeg({ quality: 70 }),
+      imageminPngquant({ speed: 5, quality: [0.6, 0.7] })
+    ])
+  )
+  .pipe(gulp.dest(distPath + 'images'));
+}
+
+export function compilePug() {
+  return gulp.src(viewsPath)
+  .pipe(pug({ pretty: true}))
+  .pipe(rename({ extname: '.html' }))
+  .pipe(inlineCss({ removeLinkTags: false, preserveMediaQueries: true  }))
+  .pipe(gulp.dest(distPath));
+}
+
+export default function () {
+  browserSync.init({
+    server: {
+      baseDir: './dist'
+    }
+  })
+
+  gulp.watch(watchedFiles, compilePug).on('change', () => {
+    browserSync.reload();
+  });
 }
---
+++ send-mail.js
@@ -0,0 +1,40 @@
+import nodemailer from 'nodemailer';
+import fs from 'fs';
+
+const yandexLogin = 'nikita-filatov51@yandex.ru';
+
+const addresses = [
+  yandexLogin,
+  'nikita45454@gmail.com',
+  'adelya0712@gmail.com',
+  'adelyalink@yandex.ru'
+]
+
+const yandex = {
+  server: 'smtp.yandex.ru',
+  ssl: true,
+  port: '465',
+  login: yandexLogin,
+  password: 'hafpczqbdazfryrq'
+}
+
+const transporter = nodemailer.createTransport({
+  host: yandex.server,
+  port: yandex.port,
+  secure: yandex.ssl,
+  auth: {
+    user: yandex.login,
+    pass: yandex.password
+  }
+});
+
+const html = fs.readFileSync('./dist/mail-main.html');
+
+const res = await transporter.sendMail({
+  from: yandexLogin,
+  to: addresses.join(', '),
+  subject: 'test mail',
+  html
+});
+
+console.log(res);
